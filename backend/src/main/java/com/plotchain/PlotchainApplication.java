42e30cac55d39e561f080d1ef68b3466a20751c1
samdarshirai <rai.samdarshi01@gmail.com>
2026-07-29 12:44:39 +0200

    feat: scaffold Spring Boot backend project

---
+++ backend/src/main/java/com/plotchain/PlotchainApplication.java
@@ -0,0 +1,11 @@
+package com.plotchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlotchainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlotchainApplication.class, args);
+    }
+}
