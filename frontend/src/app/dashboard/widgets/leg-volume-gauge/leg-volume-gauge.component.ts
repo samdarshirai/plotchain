f4afc6ecedbd8ae1dbe5b26cfb590cf3cb812229
samdarshirai <rai.samdarshi01@gmail.com>
2026-07-29 13:47:06 +0200

    fix: add i18n translation keys for leg volume labels

--- frontend/src/app/dashboard/widgets/leg-volume-gauge/leg-volume-gauge.component.ts
+++ frontend/src/app/dashboard/widgets/leg-volume-gauge/leg-volume-gauge.component.ts
@@ -9,8 +9,8 @@ import { LegVolumeSummary } from '../../models/dashboard-response.model';
   imports: [CommonModule, TranslateModule],
   template: `
     <div class="leg-volume-gauge">
-      <div class="leg left" [style.flex]="data.leftVolume || 1">L: {{ data.leftVolume | currency:'INR' }}</div>
-      <div class="leg right" [style.flex]="data.rightVolume || 1">R: {{ data.rightVolume | currency:'INR' }}</div>
+      <div class="leg left" [style.flex]="data.leftVolume || 1">{{ 'dashboard.leftLeg' | translate }}: {{ data.leftVolume | currency:'INR' }}</div>
+      <div class="leg right" [style.flex]="data.rightVolume || 1">{{ 'dashboard.rightLeg' | translate }}: {{ data.rightVolume | currency:'INR' }}</div>
       <div class="projected-match">{{ 'dashboard.projectedMatch' | translate }}: {{ data.projectedMatchAmount | currency:'INR' }}</div>
     </div>
   `
